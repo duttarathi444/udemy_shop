342aece367aaca2468735daf8e710b3e4c256210
Rathindranath Dutta <duttarathi444@gmail.com>
2020-08-14 17:16:31 +0530

    add

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var path = require('path');
 var bodyParser = require('body-parser');
 
 var app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', (req, res, next) => {
     res.send('hello World');
 })
