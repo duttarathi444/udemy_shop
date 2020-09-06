626cff22101c99391fceb90960104ce0e9b3c881
Rathindranath Dutta <duttarathi444@gmail.com>
2020-09-06 16:20:43 +0530

    all commit

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,9 @@
 exports.login = (req, res, next) => {
-    res.send('hello world');
+    res.render('add-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        // formsCSS: true,
+        // productCSS: true,
+        // activeAddProduct: true
+    });
 }
