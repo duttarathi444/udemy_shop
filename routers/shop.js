de9ca4cebff423c2d5e01f3dfeba2458e345a573
Rathindranath Dutta <duttarathi444@gmail.com>
2020-09-08 01:42:09 +0530

    change

--- routers/shop.js
+++ routers/shop.js
@@ -5,6 +5,8 @@ var router = express.Router();
 
 router.get('/', shopController.getIndex);
 
-router.get('/products',shopController.getProducts);
+router.get('/products', shopController.getProducts);
+
+router.get('/cart');
 
 module.exports = router;
